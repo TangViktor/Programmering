9b1502f09e7bd60cff0f63b465a5838e8feddf7a
Viktor Täng <viktor.tang13@gmail.com>
2024-01-22 13:46:41 +0100

    nytt

---
+++ Test 6.py
@@ -0,0 +1,15 @@
+for i in range(5):
+    print (i)
+
+
+for i in range(3, 8):
+    print (i)
+
+for i in range(4, 20, 3):
+    print (i)
+
+for i in range(20, 41, 5):
+    print (i)
+
+for i in range(10, -1, -1):
+    print (i)
